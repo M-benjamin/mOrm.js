672f1cdc1bf58c0c7cde04d2b471bbf00c59cc63
benjaminmballa <mballabenjamin@yahoo.fr>
2019-01-25 10:49:18 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,45 @@
+// // > File orm.js
+
+// import { isEmpty } from 'lodash'
+
+
+// export default class mOrm {
+//   configPathName = './morm.config.js'
+
+
+//   async createConnection(dbConfig = {}) {
+
+//   }
+// }
+
+// // > File posgres.js
+
+// import { Clien } from 'pd'
+
+// export default class Post extends Core {
+//   constructor(options) {
+//     super(options)
+//   }
+
+//   async inti() {
+//     const { hos, p, u, da } = this
+
+//     this.client = new Client({
+//       use,
+//       host,
+//       db,
+//       pass,
+//       port
+//     })
+
+//     try {
+//       await this.client.connect()
+//     } catch (error) {
+//       // > throw error
+//       throw new Error('Database does not exist')
+//     }
+
+
+
+//   }
+// }
